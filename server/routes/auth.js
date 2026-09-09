46bdcd13a7a84124564f3d714891dff3e779faea
Prem Narayani <premnarayani1682@gmail.com>
2026-09-09 14:55:56 +0530

    authorization

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/auth.controller');
+const { register, login, googleAuth } = require('../controllers/auth.controller');
 
 // Register
 router.post('/register', register);
 
 // Login
 router.post('/login', login);
+
+// Google OAuth Verification
+router.post('/google', googleAuth);
 
 module.exports = router;
